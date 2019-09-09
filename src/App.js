0ba2cde3cc6bbdb9fe24d11f4724a0ff92e476fd
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-09 15:35:27 -0700

    fix error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min. css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ToDoList from './components/ToDoList';
 

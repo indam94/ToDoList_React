e803c4e95465ad7a422759d3f305ec60c0330593
DONGGUN LEE <d0n99un17@gmail.com>
2019-09-09 16:24:12 -0700

    input component

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default class ToDoInput extends Component {
 
@@ -8,7 +9,25 @@ export default class ToDoInput extends Component {
 
         return (
             <div className= "card card-body my-3">
-                <form onSubmit={handleSubmit}></form>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className ="fas fa-book"></i>
+                            </div>
+                        </div>
+                        <input 
+                            type='text'
+                            className="form-control text-capitalize" 
+                            placeholder="add todo item"
+                            value={item}
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <button type='submit' className='btn btn-block btn-primary mt-3 text-uppercase'>
+                        add item
+                    </button>
+                </form>
             </div>
         )
     }
